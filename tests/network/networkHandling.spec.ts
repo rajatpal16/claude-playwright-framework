afde5d946848d6de3b30b38597e64e0ae2d29aec
Rajat Pal <rajatpal16@gmail.com>
2026-08-12 12:43:22 +0530

    Fix flaky dialog handling using waitForEvent, instead of page.on listener

--- tests/network/networkHandling.spec.ts
+++ tests/network/networkHandling.spec.ts
@@ -1,43 +1,26 @@
 import {test, expect } from '@playwright/test';
-test ('Wait for login API response on demoBlaze', async({page}) => {
-    await page.goto('https://www.demoblaze.com/');
-    await page.getByRole('link',{name:"Log in"}).click();
-    const responsePromise = page.waitForResponse(
-        response => response.url().includes('api.demoblaze.com/login'));
-
-    await page.locator('#loginusername').fill('test');
-    await page.locator('#loginpassword').fill('test123');
-    await page.getByRole('button',{name:'Log in'}).click();
-    const response = await responsePromise;
-    expect (response.status()).toBe(200);
-    const body = await response.json();
-    console.log('body:', body);
-    expect(body.errorMessage).toBe('Wrong password.');
-
-
-})
-
-test ('Mocked log in response', async ({page}) => {
-    await page.route('**/api.demoblaze.com/login', async(route) =>{
-        console.log('INTERCEPTED THE LOGIN REQUEST'); 
+test('Mocked log in response', async ({ page }) => {
+    await page.route('**/api.demoblaze.com/login', async (route) => {
+        console.log('INTERCEPTED THE LOGIN REQUEST');
         await route.fulfill({
-            status : 200,
-            contentType : 'application/json',
-            body: JSON.stringify({ errorMessage: 'Wrong password.'})
-        })
-    } )
-    //demoblaze displaying errror message by alert popup
-    page.on('dialog', async (dialog) =>{
-        console.log('Alert text:', dialog.message());
-        expect (dialog.message()).toContain('Wrong password.');
-        await dialog.accept();
-    })
-    //
+            status: 200,
+            contentType: 'application/json',
+            body: JSON.stringify({ errorMessage: 'Wrong password.' })
+        });
+    });
+
     await page.goto('https://www.demoblaze.com/');
-    await page.getByRole('link',{name:"Log in"}).click();
+    await page.getByRole('link', { name: "Log in" }).click();
     await page.locator('#loginusername').fill('test');
     await page.locator('#loginpassword').fill('test123');
-    await page.getByRole('button',{name:'Log in'}).click();
-    
+
+    const dialogPromise = page.waitForEvent('dialog');
+    await page.getByRole('button', { name: 'Log in' }).click();
+
+    const dialog = await dialogPromise;
+    console.log('Alert text:', dialog.message());
+    expect(dialog.message()).toContain('Wrong password.');
+    await dialog.accept();
+
     console.log('Mocked execution completed');
-})
+});
